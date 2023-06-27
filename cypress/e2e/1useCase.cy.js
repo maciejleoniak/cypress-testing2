9e8997949aa2aef3a145098c26b4ba83a490ff11
7799043 <leoniak.maciek1@gmail.com>
2023-06-27 12:26:37 +0200

    .

--- cypress/e2e/1useCase.cy.js
+++ cypress/e2e/1useCase.cy.js
@@ -1,13 +1,16 @@
+import dataSearch from '../../dataSearch.json';
+
+
 describe('User should search for an item and get appropriate results: items in result list should have query in the titles', () => {
   
   it('should display appropriate results for the query', () => {
 
-    const searchQuery = 'toys'; 
+    const searchQuery = dataSearch.searchQuery; 
 
     cy.visit('https://www.etsy.com')
 
     cy.wait(3000);
-    
+
     cy.get('div.wt-overlay__footer:nth-child(2) > div:nth-child(2) > button:nth-child(1)').click();
 
     cy.get('#global-enhancements-search-query').type(searchQuery).type('{enter}');
