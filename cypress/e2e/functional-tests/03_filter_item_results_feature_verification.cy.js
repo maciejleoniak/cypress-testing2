54b7a04239de1b21f0a9484e84312b5dba69831f
7799043 <leoniak.maciek1@gmail.com>
2023-06-29 21:39:12 +0200

    .

--- cypress/e2e/functional-tests/03_filter_item_results_feature_verification.cy.js
+++ cypress/e2e/functional-tests/03_filter_item_results_feature_verification.cy.js
@@ -1,30 +1,25 @@
-import dataSearch from '../../dataSearch.json';
-
+import dataSearch from '../../fixtures/dataSearch.json';
+import MainPage from '../../pages/mainPage';
 
 describe('User should be able to filter search results: price, shop location, color', () => {
 
     it('should be able to filter search', () => {
 
         const searchQuery = dataSearch.searchQuery;
+        const searchLocation = dataSearch.searchLocation;
+        const searchPrice = dataSearch.searchPrice;
+        const searchColor = dataSearch.searchColor;
+    
 
         cy.visit('https://www.etsy.com')
 
-        cy.wait(1000);
+        MainPage.clickOnCookieBtn();
 
-        cy.get('div.wt-overlay__footer:nth-child(2) > div:nth-child(2) > button:nth-child(1)').click();
+        MainPage.fillSearchBar(searchQuery);
 
-        cy.get('#global-enhancements-search-query').type(searchQuery).type('{enter}');
+        MainPage.clickOnSearchFilterBtn();
 
-        cy.get('#search-filter-button').click();
-
-        //shop location filter
-        cy.get('div.wt-menu__body:nth-child(1) > div:nth-child(2) > label:nth-child(2)').click();
-        // price filter
-        cy.get('.main-filters > div:nth-child(7) > fieldset:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > label:nth-child(2)').click();
-        // color filter
-        cy.get('fieldset.wt-mb-xs-2:nth-child(8) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > label:nth-child(2)').click();
-        // search btn 
-        cy.get('button.wt-btn--primary:nth-child(3)').click();
+        MainPage.clickAcceptChossenFiltersBtn(searchLocation, searchPrice, searchColor);
         
     });
 });
