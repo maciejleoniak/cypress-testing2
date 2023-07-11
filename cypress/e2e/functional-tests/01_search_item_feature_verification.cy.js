0826d8695b2ec9243ee616c23cdca5f98ba415ad
7799043 <leoniak.maciek1@gmail.com>
2023-07-11 12:26:33 +0200

    .

--- cypress/e2e/functional-tests/01_search_item_feature_verification.cy.js
+++ cypress/e2e/functional-tests/01_search_item_feature_verification.cy.js
@@ -1,26 +1,20 @@
 import dataSearch from '../../fixtures/dataSearch.json';
-import { MainPage } from '../../pages/MainPage'
+import mainpage from '../../pages/MainPage'
 
 describe('User should search for an item and get appropriate results: items in result list should have query in the titles', () => {
   it('should display appropriate results for the query', () => {
-    // Pobieranie zapytania wyszukiwania z pliku JSON
+
+
     const searchQuery = dataSearch.searchQuery;
 
-    // Tworzenie instancji strony głównej
-    const mainPage = new MainPage();
 
-    // Otwieranie strony głównej
-    mainPage.visit();
+    mainpage.GoToWebsite();
 
-    // Akceptowanie domyślnych ustawień polityki prywatności
-    mainPage.acceptDefaultPrivacyPolicySettings();
+    mainpage.PrivacyPolicySettingsAccept();
 
-    // Wykonanie wyszukiwania na stronie głównej
-    mainPage.searchFor(searchQuery);
+    mainpage.SearchBar(searchQuery);
+    
 
-    // Dodaj dalsze asercje w celu sprawdzenia wyników wyszukiwania
-    // Przykład: Sprawdzenie, czy elementy na liście wyników zawierają zapytanie w tytule
-    // expect(mainPage.getResultTitles()).to.include(searchQuery);
   });
 });
 
